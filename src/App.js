9bf27252541aee766944628abf099fac6370c657
bugNick <rlatjsgur000@naver.com>
2022-08-16 21:41:12 +0900

    study-react-13~15

--- src/App.js
+++ src/App.js
@@ -1,35 +1,76 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import Counter from "./component/Counter";
+import CreateUser from './component/CreateUser';
 import Hello from "./component/Hello";
 import InputSample from "./component/InputSample";
 import UserList from "./component/UserList";
 import Wrapper from "./component/Wrapper";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const [users, setUsers]  = useState([
     {
         id: 1,
         username: 'velopert',
-        email: 'public.velopert@gamail.com'
+        email: 'public.velopert@gamail.com',
+        active: true,
     },
     {
         id: 2,
         username: 'velopert2',
-        email: 'public.velopert2@gamail.com'
+        email: 'public.velopert2@gamail.com',
+        active: false,
     },
     {
         id: 3,
         username: 'velopert3',
-        email: 'public.velopert3@gamail.com'
+        email: 'public.velopert3@gamail.com',
+        active: false,
     },
-]
+])
 
 // useState로 관리해도 되지만 리렌더링되지 않게 변수값을 사용하고 싶을때 사용!
 const nextId = useRef(4);
 
 const onCreate = () => {
-  console.log(nextId.current); // 4
+  const user = {
+    id: nextId.current,
+    username,
+    email,
+  };
+  //setUsers([...users, user]);   spread
+  setUsers(users.concat(user)); //concat
+  setInputs({
+    username: '',
+    email: '',
+  });
+
   nextId.current += 1;
+}
+
+const onRemove = id => {
+  setUsers(users.filter(user => user.id !== id));
+}
+
+const onToggle = id => {
+  setUsers(users.map(
+    user => user.id === id
+    ? {...user, active: !user.active }
+    : user
+  ));
 }
 
   return (
@@ -42,7 +83,19 @@ const onCreate = () => {
     <br />
     <InputSample />
     <br />
-    <UserList users={users} />
+    <div>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList 
+        users={users} 
+        onRemove={onRemove}
+        onToggle={onToggle}
+      />
+    </div>
   </>
   );
 }
--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,20 +1,37 @@
 import React from 'react'
 
-function User({ user }) {
+function User({ user, onRemove,  onToggle }) {
+    const { username, email, id, active} = user
     return (
         <div>
-        <b>{user.username}</b><span>{user.email}</span>
+        <b style={{
+            color: active ? 'green' : 'black',
+            cursor: 'pointer'
+            }}
+            onClick={ () =>onToggle(id)}
+        >
+            {username}
+        </b>
+        &nbsp;
+        <span>{email}</span>
+        <button onClick={ () => onRemove(id)}>삭제</button>
         </div>
     )
 }
 
-function UserList({users}) {
-    
+function UserList({ users, onRemove, onToggle }) {
   return (
     <>
         {
             users.map(
-                user => (<User key={user.id} user={user} />)
+                user => (
+                <User 
+                    key={user.id} 
+                    user={user} 
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
+                )
             )
         }
     </>
