76218e7ecfd6905bab54ab1c9b32c4bbe4d5ca6e
bugNick <rlatjsgur000@naver.com>
2022-08-17 22:52:54 +0900

    study-react-16

--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function User({ user, onRemove,  onToggle }) {
     const { username, email, id, active} = user
+    // 첫번째 인자는 실행 할 함수
+    // props로 받아온 값들은 useEffect 내에서 사용할 땐 deps에 넣기
+    /*
+    useEffect( () => {
+        console.log('컴포넌트가 화면에 나타남')
+        // props -> state
+        // REST API
+        // D3 Video.js 등 라이브러리
+        // setInterval, setTimeout
+        return () => {
+            // clearInterval, clearTimeout
+            // 라이브러리 인스턴스 제거
+            console.log('컴포넌트가 화면에서 사라짐')
+        }
+    },[])
+    */
+    useEffect( () => {
+        console.log('설정됨')
+        console.log(user);
+        return () => {
+            console.log('바뀌기 전')
+            console.log(user);
+        }
+    }, [user]);
+
     return (
         <div>
         <b style={{
